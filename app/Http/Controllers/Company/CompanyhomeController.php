56bc67fda07a31301e294efbaaa61d8c4de17558
root <root@localhost.localdomain>
2022-02-25 16:18:06 +0000

    hhs

--- app/Http/Controllers/Company/CompanyhomeController.php
+++ app/Http/Controllers/Company/CompanyhomeController.php
@@ -9,7 +9,7 @@ class CompanyhomeController extends Controller
 {
     public function Companyhome(){
 
-        $data = Course::where('courses.status',0)->paginate(6);
+        $data = Course::where('status',1)->paginate(6);
         return view('company.company_home',compact('data'));
     }
 }
