c5ff9f46c876fdd3f09a0eb45ec46aa729c62fa4
GraceCoders <nb.gorilladevelopment@gmail.com>
2022-04-01 19:23:18 +0530

    update

--- database/migrations/2022_04_01_134917_add_new_column_purcshe_couser.php
+++ database/migrations/2022_04_01_134917_add_new_column_purcshe_couser.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeColumnPurchasesCourseYabel extends Migration
+class AddNewColumnPurcsheCouser extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class ChangeColumnPurchasesCourseYabel extends Migration
      */
     public function up()
     {
-        Schema::table('purchase_course', function($table) {
-            $table->dropColumn('status');
+        Schema::create('purchase_course', function (Blueprint $table) {
+            $table->string('status')->nullable();
         });
     }
 
@@ -25,8 +25,6 @@ class ChangeColumnPurchasesCourseYabel extends Migration
      */
     public function down()
     {
-        Schema::table('purchase_course', function($table) {
-            $table->dropColumn('status');
-        });
+        //
     }
 }
