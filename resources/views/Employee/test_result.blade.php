399e571537d95bb8543b2fa1dc856651cc2035fc
GraceCoders <nb.gorilladevelopment@gmail.com>
2022-04-04 22:10:56 +0530

    update

--- resources/views/Employee/test_result.blade.php
+++ resources/views/Employee/test_result.blade.php
@@ -26,7 +26,7 @@
                 </h4>
                 <p>You have earned a certification for this course. </p>
                 <a href="{{route('create.pdf',$id)}}"><button type="button" class="btn btn btn-lg" id="download_certificate">Download certificate</button></a>
-                        <p class="back_pera"><a href="{{url('employee/home')}}" class="back_pera">Back to home</a></p>
+                        <p class="back_pera"><a href="{{url('employee/courses')}}" class="back_pera">Back to home</a></p>
                      </div>
                 </div>
 </section>
