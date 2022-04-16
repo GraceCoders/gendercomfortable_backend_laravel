4c315ce80381e863480f82a0573745ebfe911925
GraceCoders <nb.gorilladevelopment@gmail.com>
2022-04-16 14:28:12 +0530

    update

--- resources/views/Employee/layouts/header.blade.php
+++ resources/views/Employee/layouts/header.blade.php
@@ -9,15 +9,21 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/slicing_web/logo.svg') }}">
 
 
-    <link rel="stylesheet" href="{{asset('assets/css/style2.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style2.css') }}">
+
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no">
     <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 @php
 $user = Auth::user();
@@ -27,23 +33,27 @@ $user = Auth::user();
         <div class="container">
             <div class="clearfix">
                 <div class="float-end">
-                    <a href=""> <img class="image" src="{{asset('assets/slicing_web/mail.svg')}}" class="img-fluid" alt="..."></a>
+                    <a href=""> <img class="image-fluid" src="{{ asset('assets/slicing_web/mail.svg') }}"
+                            class="img-fluid" alt="..."></a>
                     </a>
-                    <p>support@gendercomfortable.com</p>
-                    <a href=""><img class="image" src="{{asset('assets/slicing_web/fb.svg')}}" class="img-fluid" alt="..."></a>
-                    <a href=""><img class="image" src="{{asset('assets/slicing_web/insta.svg')}}" class="img-fluid" alt="..."></a>
-                    <a href=""><img class="image" src="{{asset('assets/slicing_web/linkedin.svg')}}" class="img-fluid" alt="...">
+       <p>support@gendercomfortable.com</p>
+                    <a href=""><img class="image-fluid" src="{{ asset('assets/slicing_web/fb.svg') }}"
+                            class="img-fluid" alt="..."></a>
+                    <a href=""><img class="image-fluid" src="{{ asset('assets/slicing_web/insta.svg') }}"
+                            class="img-fluid" alt="..."></a>
+                    <a href=""><img class="image-fluid" src="{{ asset('assets/slicing_web/linkedin.svg') }}"
+                            class="img-fluid" alt="...">
                     </a>
                 </div>
             </div>
         </div>
     </section>
- <!-- second div start -->
- <section class="bg-light">
+<!-- second div start -->
+<section class="bg-light">
     <div class="container">
-    <div class="row ">
+        <div class="row ">
             <div class=" col-lg-2 col-12" id="second">
-                <img class="img-fluid" src="{{asset('assets/slicing_web/logo.svg')}}" class="img-fluid" alt="...">
+               <img class="img-fluid" src="{{asset('assets/slicing_web/logo.svg')}}" class="img-fluid" alt="...">
             </div>
             <div class="col-lg-10 col-12" id="second">
                 <div class="buttons1" style="display:inline-flex">
@@ -51,59 +61,89 @@ $user = Auth::user();
                         <button type="button" class="btn btn bg-light dropdown-toggle" id="dropdownMenuButton11" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
                             <img class="image" src="{{asset('assets/slicing_web/notification.svg')}}" class="img-fluid" alt="...">
                         </button>
+                        <ul class="dropdown-menu dropdown-menu-end ">
+                            <li class="dropdown_position">
+                                <div class="modal-heads">
+                            </li>
+                            <li>
+                                <div class="notification">
+                                    <div class=" container ">
+                                        <div class="row">
+                                            <div class="col-lg-2 col-12">
+                                                <img class="img-fluid" id="image_radius" src="{{asset('assets/slicing_web/dummy_img3.png')}}" alt="..."></div>
+                                            <div class="col-lg-10 col-12">
+                                                <p class="donec1 "> Lisa Hyden has redeemed your course “Lorem ipsum dolor sit amet”
+                                                </p>
+                                                <p class="float-end" id="min_ago">2min .ago</p>
+                                            </div>
+                                        </div>
+                                        <hr class="line "></hr>
+                                        <div class="row mt-3">
+                                            <div class="col-lg-2 col-12 ">
+                                                <img class="img-fluid" id="image_radius" src="{{asset('assets/slicing_web/dummy_img1.png')}}" alt="..."></div>
+                                            <div class="col-lg-10 col-12">
+                                                <p class="donec1 ">John Doe has redeemed your course “Lorem ipsum dolor sit amet”
+                                                </p>
+                                                <p class="float-end" id="min_ago">2min .ago</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
 
+                        </ul>
                         </div>
+
+
                         @if( $user->profile_pic)
                         <img class="image-fluide" src="{{ asset('storage/' . $user->profile_pic) }}"
                             class="img-fluid" id="profilepic" alt="...">      
                                                     @else
                               <img class="img-fluid" src="{{ asset('assets/slicing_web/profile.png') }}" class="img-fluid" id="profilepic" alt="..." id="roundimgs">
-                              @endif 
-                        <div class="btn-group">
+                              @endif                       <div class="btn-group">
                             <button type="button" class="btn btn bg-light dropdown-toggle"  id="dropdownMenuButton11" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
                                 <img class="image" src="{{asset('assets/slicing_web/dropdown.svg')}}" class="img-fluid" alt="...">
                             </button>
-                            <ul class="dropdown-menu dropdown-menu-end " id="dropdown-menu2">
+                            <ul class="dropdown-menu  dropdown-menu-end" id="dropdown-menu2">
                                 <li class="dropdown_position">
                                     <div class="modal-heads">
                                         <div class="spacing11">
-                                            <h5 class="modal-title" id="exampleModalLabel">Hello, {{$user->fisrt_name.' '.$user->last_name}}!</h5>
-                                           
+                                            <h5 class="modal-title" id="exampleModalLabel">Hello {{$user->username}}!</h5>
+
                                         </div>
                                         <hr class="line"></hr>
                                     </div>
                                 </li>
                                 <div class="spacing">
                                     <li>
-                                        <a class="dropdown-item" href="{{route('employee.home')}}">
+                                        <a class="dropdown-item" href="{{route('company.home')}}">
                                             <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/home.svg')}}" alt="..."> &nbsp; Home</p>
                                         </a>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="{{ route('employee.edit.profile') }}">
-                                            <p class="donec1"><img class="img-fluid" id="edit_img"
-                                                    src="{{ asset('assets/slicing_web/edit.svg') }}"
-                                                    alt="...">&nbsp;&nbsp;&nbsp;&nbsp;Edit Profile</p>
+                                        <a class="dropdown-item" href="{{url('company/profile')}}">
+                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/edit.svg')}}"> &nbsp;&nbsp;&nbsp;Edit Profile</p>
                                         </a>
                                     </li>
+                                    <!-- <li>
+                                        <a class="dropdown-item" href="{{url('company/payment_option')}}">
+                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/payment.svg')}}" alt="..."> &nbsp; Payment Option</p>
+                                        </a>
+                                    </li> -->
                                     <li>
-                                        <a class="dropdown-item" href="{{url('employee/courses')}}">
-                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/purchased.svg')}}" alt="..."> &nbsp;Courses</p>
+                                        <a class="dropdown-item" href="{{url('/company/purchased_courses')}}">
+                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/purchased.svg')}}" alt="..."> &nbsp; Purchased Courses</p>
                                         </a>
                                     </li>
+
                                     <li>
-                                        <a class="dropdown-item" href="{{url('employee/certification')}}">
-                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/certification.svg')}}" alt="..."> &nbsp;Certifications</p>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="{{url('change_password')}}">
-                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/card.svg')}}" alt="..."> &nbsp;Change Password</p>
+                                        <a class="dropdown-item" href="{{route('company.change.password')}}">
+                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/pass.svg')}}" alt="..."> &nbsp;&nbsp;&nbsp;Change Password</p>
                                         </a>
                                     </li>
                                     <li>
                                         <a class="dropdown-item" href="#">
-                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/terms.svg')}}" alt="..."> &nbsp;Terms&conditions</p>
+                                            <p class="donec1"><img class="img-fluid" class="image" src="{{asset('assets/slicing_web/terms.svg')}}" alt="..."> &nbsp&nbsp;Terms&conditions</p>
                                         </a>
                                     </li>
                                     <li>
@@ -116,6 +156,7 @@ $user = Auth::user();
                                 <hr class="line"></hr>
                                 <li>
                                     <div class="modal-footers text-center ">
+
                                         <p class="logout_popup">
                                             <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
@@ -134,7 +175,8 @@ $user = Auth::user();
                     </div>
                 </div>
             </div>
-    </div>
+
         </div>
+
 </section>
 <section class="bg-light header_border "></section>
